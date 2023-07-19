9cb5757050c6384a8b28cf30afd82ff1ecdecb7c
surfacez <sergiobatistajr94@gmail.com>
2023-07-19 16:29:09 -0300

    trying fix cache

--- actions/getInsiders.tsx
+++ actions/getInsiders.tsx
@@ -3,7 +3,7 @@ import { Guest } from "@prisma/client";
 const URL = `${process.env.NEXT_PUBLIC_API}/api/portaria/insiders`;
 
 const getInsiders = async (): Promise<Guest[]> => {
-  const res = await fetch(URL);
+  const res = await fetch(URL, { next: { revalidate: 0 } });
 
   return res.json();
 };
