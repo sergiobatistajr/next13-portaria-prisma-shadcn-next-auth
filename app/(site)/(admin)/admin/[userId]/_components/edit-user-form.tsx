23d68d558ce387d3b920e3e10c2f1069cdf54742
surfacez <sergiobatistajr94@gmail.com>
2023-08-03 11:43:41 -0300

    Revert "remove zustand and fix the reset password"
    This reverts commit d78e87b3c212c51816a750cd168db82fc3903fe7.

--- app/(site)/(admin)/admin/[userId]/_components/edit-user-form.tsx
+++ app/(site)/(admin)/admin/[userId]/_components/edit-user-form.tsx
@@ -32,54 +32,26 @@ import { Checkbox } from "@/components/ui/checkbox";
 
 const roles = ["admin", "porteiro", "relatorio"];
 
-const formSchema = z
-  .object({
-    name: z.string().min(3, "Nome muito curto"),
-    username: z.string().min(3, "Usuário muito curto"),
-    role: z.enum(["admin", "porteiro", "relatorio"]),
-    isActive: z.boolean(),
-    password: z.string().optional(),
-    confirmPassword: z.string().optional(),
-  })
-  .refine(
-    (data) => {
-      if (data.password && data.confirmPassword) {
-        return data.password === data.confirmPassword;
-      }
-      return true;
-    },
-    {
-      message: "As senhas não são iguais",
-      path: ["confirmPassword"],
-    }
-  );
+const formSchema = z.object({
+  name: z.string().min(3, "Nome muito curto"),
+  username: z.string().min(3, "Usuário muito curto"),
+  role: z.enum(["admin", "porteiro", "relatorio"]),
+  isActive: z.boolean(),
+});
 interface EditUserFormProps {
-  initialValues: Pick<
-    User,
-    "id" | "name" | "username" | "role" | "isActive"
-  > & {
-    password?: string;
-    confirmPassword?: string;
-  };
+  initialValues: User;
 }
 
 const EditUser: React.FC<EditUserFormProps> = ({ initialValues }) => {
   const [isLoading, setIsLoading] = useState(false);
-  const [isShow, setIsShow] = useState(false);
   const router = useRouter();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      ...initialValues,
-      password: "",
-      confirmPassword: "",
-    } || {
+    defaultValues: initialValues || {
       name: "",
       username: "",
       role: "relatorio",
       isActive: true,
-      password: "",
-      confirmPassword: "",
     },
   });
 
@@ -101,129 +73,86 @@ const EditUser: React.FC<EditUserFormProps> = ({ initialValues }) => {
     <Container>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)}>
-          {!isShow && (
-            <div className="pt-5 md:grid md:grid-cols-4 gap-4 mb-10">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Nome</FormLabel>
+          <div className="pt-5 md:grid md:grid-cols-4 gap-4 mb-10">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Nome completo" {...field} />
+                  </FormControl>
+                  <FormDescription>Este é seu nome completo</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Usuário</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Seu usuário" {...field} />
+                  </FormControl>
+                  <FormDescription>Este é seu usuário</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="role"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Função</FormLabel>
+                  <Select
+                    // @ts-ignore
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
                     <FormControl>
-                      <Input placeholder="Nome completo" {...field} />
+                      <SelectTrigger>
+                        <SelectValue placeholder="Seleciona sua função" />
+                      </SelectTrigger>
                     </FormControl>
-                    <FormDescription>Este é seu nome completo</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Usuário</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Seu usuário" {...field} />
-                    </FormControl>
-                    <FormDescription>Este é seu usuário</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="role"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Função</FormLabel>
-                    <Select
+                    <SelectContent>
+                      {roles.map((role) => (
+                        <SelectItem key={role} value={role}>
+                          {role}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormDescription>Esta é sua função</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="isActive"
+              render={({ field }) => (
+                <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+                  <FormControl>
+                    <Checkbox
+                      checked={field.value}
                       // @ts-ignore
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
-                      <FormControl>
-                        <SelectTrigger>
-                          <SelectValue placeholder="Seleciona sua função" />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        {roles.map((role) => (
-                          <SelectItem key={role} value={role}>
-                            {role}
-                          </SelectItem>
-                        ))}
-                      </SelectContent>
-                    </Select>
-                    <FormDescription>Esta é sua função</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="isActive"
-                render={({ field }) => (
-                  <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
-                    <FormControl>
-                      <Checkbox
-                        checked={field.value}
-                        // @ts-ignore
-                        onCheckedChange={field.onChange}
-                      />
-                    </FormControl>
-                    <div className="space-y-1 leading-none">
-                      <FormLabel>Ativo</FormLabel>
-                      <FormDescription>
-                        Selecione se o usuário está ativo ou inativo
-                      </FormDescription>
-                    </div>
-                  </FormItem>
-                )}
-              />
-            </div>
-          )}
-          <Button
-            variant="link"
-            type="button"
-            disabled={isLoading}
-            onClick={() => setIsShow(!isShow)}
-          >
-            {isShow ? "Ocultar" : "Mostrar"} Resetar senha
-          </Button>
-          {isShow && (
-            <div className="pt-5 md:grid md:grid-cols-4 gap-4 mb-10">
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Nova senha</FormLabel>
-                    <FormControl>
-                      <Input type="password" {...field} />
-                    </FormControl>
-                    <FormDescription>Está é sua senha</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="confirmPassword"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Confirme a nova senha</FormLabel>
-                    <FormControl>
-                      <Input type="password" {...field} />
-                    </FormControl>
-                    <FormDescription>Confirme a senha</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-          )}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                  <div className="space-y-1 leading-none">
+                    <FormLabel>Ativo</FormLabel>
+                    <FormDescription>
+                      Selecione se o usuário está ativo ou inativo
+                    </FormDescription>
+                  </div>
+                </FormItem>
+              )}
+            />
+          </div>
           <div className="space-x-2 flex items-center justify-start w-full">
             <Button disabled={isLoading} type="submit">
               Cadastar
